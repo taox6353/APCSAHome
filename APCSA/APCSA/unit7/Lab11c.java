52254974755407d7d814677f2b67aba5d1dd4812
Tao <Tao@Tao-VAIO>
2018-02-21 23:58:51 -0800

    Finished Lab11c

--- APCSA/APCSA/unit7/Lab11c.java
+++ APCSA/APCSA/unit7/Lab11c.java
@@ -6,8 +6,12 @@ public class Lab11c
 {
    public static void main( String args[] )
    {
-
-
+	   TriangleThree tt = new TriangleThree();
+	   
+	   
+	   tt.setTriangle("A", 3);
+	   tt.setTriangle("X", 7);
+	   tt.setTriangle("R", 1);
 
 
 
