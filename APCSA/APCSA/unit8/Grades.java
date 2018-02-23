b1b39798e54b1b61df6fd71460f35110089883a9
Tao <Tao@Tao-VAIO>
2018-02-22 19:30:44 -0800

    Finished Lab14d

---
+++ APCSA/APCSA/unit8/Grades.java
@@ -0,0 +1,55 @@
+package unit8;
+
+import java.lang.System;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Grades
+{
+	//instance variables
+	
+	double[] grades;
+	int arraySize;
+
+	//constructor
+	public Grades()
+	{
+		arraySize = 0;
+	}
+	//set method
+	public void setGrades(double[] grad)
+	{
+		grades = grad;
+	}
+	private double getSum()
+	{
+		double sum=0.0;
+		
+		for(int i=0;i<grades.length;i++){
+			sum = sum + grades[i];
+		}
+
+		return sum;
+	}
+
+	public double getAverage()
+	{
+		double average=0.0;
+		average = getSum()/grades.length;
+		
+		return average;
+	}
+	public String toString()//toString method
+	{
+		String print = "";
+		
+		for(double i=0;i<grades.length;i++){
+			print = print + "grade "+ (int)i +" :: "+ grades[(int)i]+"\n";
+		}
+		print = print + "\naverage = " + getAverage();
+		
+		return print;
+	}
+
+
+}
---
+++ APCSA/APCSA/unit8/Lab14d.java
@@ -0,0 +1,16 @@
+package unit8;
+
+public class Lab14d
+{
+	public static void main( String args[] )
+	{
+		Grades gd = new Grades();
+		
+		//add test cases
+		
+		double[] grad = {100, 90, 85, 72.5, 95.6};
+		gd.setGrades(grad);
+		System.out.println(gd);
+		
+	}
+}
