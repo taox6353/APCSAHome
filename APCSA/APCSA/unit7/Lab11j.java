8647ad343e0036e67b24ddeb428e7f59dec7182c
Tao <Tao@Tao-VAIO>
2018-02-22 16:45:22 -0800

    Finished Lab11j

---
+++ APCSA/APCSA/unit7/Lab11j.java
@@ -0,0 +1,13 @@
+package unit7;
+
+import static java.lang.System.*;
+
+public class Lab11j
+{
+   public static void main(String args[])
+   {
+	   Triples tp = new Triples(110);
+	   System.out.println(tp);
+	   
+	}
+}
--- APCSA/APCSA/unit7/Triples.java
+++ APCSA/APCSA/unit7/Triples.java
@@ -1,6 +1,7 @@
 package unit7;
 
-import static java.lang.System.*;
+import java.lang.System.*;
+import java.lang.Math;
 
 public class Triples
 {
@@ -13,34 +14,46 @@ public class Triples
 
 	public Triples(int num)
 	{
-
-
+		number = num;
 	}
 
 	public void setNum(int num)
 	{
-
-
+		number = num;
+		toString();
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		int max = 0;
-
-
-
-		return 1;
+		int max = 1;
+		
+		for(int i=2;i<number;i++){
+		  if(a%i==0 && b%i==0 && c%i==0){
+		    max = 0;
+		  }
+		}
+		if (max == 0)
+		    return 0;
+		  else
+		    return 1;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
+		
+		for(int a=1;a<=number;a++){
+		  for (int b=1;b<=number;b++){
+		    for (int c=1;c<=number;c++){
+		      if (a<b && a<c && Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2) && c%2==1 && greatestCommonFactor(a,b,c)==1){
+		        if ((a%2==1 && b%2==0)||(a%2==0 && b%2==1)){
+		            output = output + a +" "+  b +" "+ c+ "\n";
+		        }
+		      }
+		      
+		    }
+		  }
+		}
 		return output+"\n";
 	}
 }
