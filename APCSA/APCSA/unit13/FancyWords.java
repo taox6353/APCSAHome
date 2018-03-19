ea7e00467066bd19ba3e0e851e9ce92637b1c158
Tao <Tao@Tao-VAIO>
2018-03-19 01:33:05 -0700

    Finished Lab15d

---
+++ APCSA/APCSA/unit13/FancyWords.java
@@ -0,0 +1,50 @@
+package unit13;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+import static java.lang.System.*;
+
+import java.io.File;
+
+public class FancyWords
+{
+	private String[] wordRay;
+
+	public FancyWords(String sentence)
+	{
+		setWords(sentence);
+	}
+
+	public void setWords(String sentence)
+	{
+		
+		wordRay = sentence.split(" ");
+		
+	}
+
+	public String toString()
+	{
+		String output="";
+		int max = Integer.MIN_VALUE;
+		
+		
+		Arrays.toString(wordRay);
+		//System.out.println("---"+wordRay[0]+"---");
+		//The following is extremely problematic
+		for(int m=0;m<wordRay.length;m++){
+			if(wordRay[m].length()>max)
+				max = wordRay[m].length();
+		}
+		for(int j=0;j<max;j++){
+			for(int i=wordRay.length-1;i>=0;i--){
+				if(1+j>wordRay[i].length())
+					output = output + " ";
+				else
+					output = output + wordRay[i].charAt(wordRay[i].length()-(1+j));
+			}
+			output = output + "\n";
+		}
+		return output+"\n\n";
+	}
+}
